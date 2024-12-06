461ba3b1e0778cc08e0584a2df7eab102a9a6daa
Jaydaaah <sv.jdrapha@gmail.com>
2024-12-07 01:58:10 +0800

    lazy load exam

--- resources/js/Pages/Exam/Exam.tsx
+++ resources/js/Pages/Exam/Exam.tsx
@@ -1,100 +1,11 @@
-import ChatInput from "@/Components/TextArea/ChatInput";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
-import { GroqPrompt } from "@/lib/Groq-api";
-import { PageProps } from "@/types";
-import { Chat } from "@/types/models";
-import { Head, router, useForm } from "@inertiajs/react";
-import axios from "axios";
-import { FormEvent, useCallback, useEffect, useMemo, useState } from "react";
-import RenderedMessage from "./partial/RenderedMessage";
-import ExamStart from "./partial/ExamStart";
-import TypingResponse from "./partial/TypingResponse";
+import { Head } from "@inertiajs/react";
 import ExamHeader from "./partial/ExamHeader";
+import { lazy, Suspense } from "react";
 
-export default function Exam({
-    chats,
-}: PageProps<{
-    chats: Chat[];
-    api: {
-        key: string;
-        provider: string;
-    };
-}>) {
-    const [temporaryEnd, setEnd] = useState(false);
-    const [tempStreamDonetext, setTempStreamDonetext] = useState("");
-    const { data, setData, post, reset, processing, errors } = useForm({
-        prompt: "",
-    });
+const ExamComponent = lazy(() => import("./LazyLoadExam"));
 
-    const syncResponse = useCallback(() => {
-        axios
-            .post(route("exam.ai.sync"), {
-                data: {
-                    message: tempStreamDonetext,
-                },
-            })
-            .finally(() => {
-                setTempStreamDonetext("");
-                router.reload();
-            });
-    }, [tempStreamDonetext]);
-
-    const onSuccess = useCallback(async () => {
-        const response = await GroqPrompt(data.prompt, chats);
-        setTempStreamDonetext(response);
-        reset("prompt");
-    }, [data, chats]);
-
-    const onSubmit = useCallback(
-        (event: FormEvent<HTMLFormElement>) => {
-            event.preventDefault();
-            if (data.prompt.length > 0) {
-                post(route("exam.edit"), {
-                    onSuccess,
-                });
-            }
-        },
-        [data, onSuccess, post]
-    );
-
-    const renderMessages = useMemo(() => {
-        return chats.map(({ side, message, created_at }, index) => {
-            const name = side == "right" ? "You" : "AI";
-            const messageLower = message.toLowerCase().trim();
-
-            const time = new Date(created_at);
-
-            if (name == "You" && messageLower == "end") {
-                setEnd(true);
-            }
-            return (
-                <RenderedMessage
-                    key={`message-${index}`}
-                    message={message}
-                    side={(side == "right" && "right") || "left"}
-                    time={time}
-                />
-            );
-        });
-    }, [chats]);
-
-    const renderTypingResponse = useMemo(() => {
-        if (tempStreamDonetext) {
-            return (
-                <TypingResponse
-                    syncResponse={syncResponse}
-                    tempStreamDonetext={tempStreamDonetext}
-                />
-            );
-        }
-    }, [tempStreamDonetext, syncResponse]);
-
-    useEffect(() => {
-        if (renderMessages.length <= 0) {
-            setData("prompt", "Start");
-        }
-    }, [renderMessages]);
-
+export default function Exam() {
     return (
         <Authenticated header={<ExamHeader />}>
             <Head title="Exam" />
@@ -105,34 +16,9 @@ export default function Exam({
                         Topic: Programming Languages and Execution Models
                     </h2>
                 </div>
-
-                {renderMessages.length > 0 ? (
-                    <div className="flex-grow w-full max-w-4xl space-y-6 sm:px-6 lg:px-8 flex flex-col-reverse">
-                        <ChatInput
-                            value={data.prompt}
-                            disabled={temporaryEnd}
-                            processing={processing}
-                            onSubmit={onSubmit}
-                            onChange={({ target }) => {
-                                setData("prompt", target.value);
-                            }}
-                        />
-                        <div className="flex-grow flex flex-col-reverse gap-5 h-[75vh] overflow-x-hidden overflow-y-scroll px-5 scrollbar py-4">
-                            {renderTypingResponse}
-                            {renderMessages}
-                        </div>
-                    </div>
-                ) : (
-                    <ExamStart
-                        onClick={() => {
-                            if (data.prompt == "Start") {
-                                post(route("exam.edit"), {
-                                    onSuccess,
-                                });
-                            }
-                        }}
-                    />
-                )}
+                <Suspense fallback={<div className="h-[80vh]">Loadingg exam...</div>}>
+                    <ExamComponent />
+                </Suspense>
             </div>
         </Authenticated>
     );
---
+++ resources/js/Pages/Exam/LazyLoadExam.tsx
@@ -0,0 +1,131 @@
+import ChatInput from "@/Components/TextArea/ChatInput";
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+import { GroqPrompt } from "@/lib/Groq-api";
+import { PageProps } from "@/types";
+import { Chat } from "@/types/models";
+import { Head, router, useForm, usePage } from "@inertiajs/react";
+import axios from "axios";
+import { FormEvent, useCallback, useEffect, useMemo, useState } from "react";
+import RenderedMessage from "./partial/RenderedMessage";
+import ExamStart from "./partial/ExamStart";
+import TypingResponse from "./partial/TypingResponse";
+import ExamHeader from "./partial/ExamHeader";
+
+export default function LazyLoadExam() {
+    const { props } = usePage<
+        PageProps<{
+            chats: Chat[];
+            api: {
+                key: string;
+                provider: string;
+            };
+        }>
+    >();
+    const { chats } = props;
+    const [temporaryEnd, setEnd] = useState(false);
+    const [tempStreamDonetext, setTempStreamDonetext] = useState("");
+    const { data, setData, post, reset, processing, errors } = useForm({
+        prompt: "",
+    });
+
+    const syncResponse = useCallback(() => {
+        axios
+            .post(route("exam.ai.sync"), {
+                data: {
+                    message: tempStreamDonetext,
+                },
+            })
+            .finally(() => {
+                setTempStreamDonetext("");
+                router.reload();
+            });
+    }, [tempStreamDonetext]);
+
+    const onSuccess = useCallback(async () => {
+        const response = await GroqPrompt(data.prompt, chats);
+        setTempStreamDonetext(response);
+        reset("prompt");
+    }, [data, chats]);
+
+    const onSubmit = useCallback(
+        (event: FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+            if (data.prompt.length > 0) {
+                post(route("exam.edit"), {
+                    onSuccess,
+                });
+            }
+        },
+        [data, onSuccess, post]
+    );
+
+    const renderMessages = useMemo(() => {
+        return chats.map(({ side, message, created_at }, index) => {
+            const name = side == "right" ? "You" : "AI";
+            const messageLower = message.toLowerCase().trim();
+
+            const time = new Date(created_at);
+
+            if (name == "You" && messageLower == "end") {
+                setEnd(true);
+            }
+            return (
+                <RenderedMessage
+                    key={`message-${index}`}
+                    message={message}
+                    side={(side == "right" && "right") || "left"}
+                    time={time}
+                />
+            );
+        });
+    }, [chats]);
+
+    const renderTypingResponse = useMemo(() => {
+        if (tempStreamDonetext) {
+            return (
+                <TypingResponse
+                    syncResponse={syncResponse}
+                    tempStreamDonetext={tempStreamDonetext}
+                />
+            );
+        }
+    }, [tempStreamDonetext, syncResponse]);
+
+    useEffect(() => {
+        if (renderMessages.length <= 0) {
+            setData("prompt", "Start");
+        }
+    }, [renderMessages]);
+
+    return (
+        <>
+            {renderMessages.length > 0 ? (
+                <div className="flex-grow w-full max-w-4xl space-y-6 sm:px-6 lg:px-8 flex flex-col-reverse">
+                    <ChatInput
+                        value={data.prompt}
+                        disabled={temporaryEnd}
+                        processing={processing}
+                        onSubmit={onSubmit}
+                        onChange={({ target }) => {
+                            setData("prompt", target.value);
+                        }}
+                    />
+                    <div className="flex-grow flex flex-col-reverse gap-5 h-[75vh] overflow-x-hidden overflow-y-scroll px-5 scrollbar py-4">
+                        {renderTypingResponse}
+                        {renderMessages}
+                    </div>
+                </div>
+            ) : (
+                <ExamStart
+                    onClick={() => {
+                        if (data.prompt == "Start") {
+                            post(route("exam.edit"), {
+                                onSuccess,
+                            });
+                        }
+                    }}
+                />
+            )}
+        </>
+    );
+}
